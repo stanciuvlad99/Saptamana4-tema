d606aa679bd6a73781fadb7368b82f3b71de9af9
stanciuvlad99 <stanciuvlad99@yahoo.com>
2022-07-27 14:00:18 +0300

    Adaugat clase + update

--- src/test/java/WebTabTable.java
+++ src/test/java/WebTabTable.java
@@ -1,6 +1,20 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebTabTable {
 
     public static void main(String[] args) {
+        ChromeDriver driver = createChromeDtiverAndPage();
+        x(driver);
 
+    }
+
+    public static ChromeDriver createChromeDtiverAndPage(){
+        ChromeDriver driver = WebDriverManager.createChromeDriver();
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+        return driver;
+    }
+
+    public static void x(ChromeDriver driver){
+        System.out.println("");
     }
 }
