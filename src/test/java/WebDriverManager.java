1d824ad403136e69e731ad1406706b3e88b90683
stanciuvlad99 <stanciuvlad99@yahoo.com>
2022-07-27 14:34:45 +0300

    Update

--- src/test/java/WebDriverManager.java
+++ src/test/java/WebDriverManager.java
@@ -2,9 +2,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverManager {
 
-    public static ChromeDriver createChromeDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+    public static ChromeDriver chromeDriver(){
+        System.setProperty("webdriver.chrome.driver" , "src/test/resources/chromedriver.exe");
         return new ChromeDriver();
+
 
     }
 }
--- src/test/java/WebTabTable.java
+++ src/test/java/WebTabTable.java
@@ -1,20 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebTabTable {
 
     public static void main(String[] args) {
-        ChromeDriver driver = createChromeDtiverAndPage();
-        x(driver);
+        ChromeDriver driver = ChromeDriverAndPage();
+        basicWebPageExample(driver);
 
     }
 
-    public static ChromeDriver createChromeDtiverAndPage(){
-        ChromeDriver driver = WebDriverManager.createChromeDriver();
+    public static ChromeDriver ChromeDriverAndPage(){
+        ChromeDriver driver = WebDriverManager.chromeDriver();
         driver.get("https://testpages.herokuapp.com/styled/index.html");
         return driver;
+
     }
 
-    public static void x(ChromeDriver driver){
-        System.out.println("");
+    public static void basicWebPageExample(ChromeDriver driver){
+        WebElement basicWebPageExample = driver.findElement(By.id("basicpagetest"));
+        basicWebPageExample.click();
+        WebElement text = driver.findElement(By.id("para1"));
+        System.out.println(text.getText());
+        WebElement text1 = driver.findElement(By.id("para2"));
+        System.out.println(text1.getText());
     }
 }
