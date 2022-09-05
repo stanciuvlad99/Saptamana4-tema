e8b72f772a8f60796c0dc0f949f330aa773d96ab
stanciuvlad99 <stanciuvlad99@yahoo.com>
2022-09-05 18:31:44 +0300

    Selenium Grid

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+
+public class BaseTest {
+
+    protected RemoteWebDriver driver = null;
+
+
+
+    public void beforeMethod() throws MalformedURLException {
+        driver=WebDriverManager.getRemoteWebDriver();
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+    }
+}
+
