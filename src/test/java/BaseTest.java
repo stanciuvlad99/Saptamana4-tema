7b7ef90a6fde1b45bd95e755dbba57cceba30595
stanciuvlad99 <stanciuvlad99@yahoo.com>
2022-09-05 18:57:40 +0300

    Selenium Grid

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,7 +6,6 @@ public class BaseTest {
 
     protected RemoteWebDriver driver = null;
 
-
     public void beforeMethod() throws MalformedURLException {
         driver=WebDriverManager.getRemoteWebDriver();
         driver.get("https://testpages.herokuapp.com/styled/index.html");
