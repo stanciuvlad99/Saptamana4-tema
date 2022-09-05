b1c775f98c0524ec390f62876b8ae02eb22f125c
stanciuvlad99 <stanciuvlad99@yahoo.com>
2022-09-05 18:30:26 +0300

    Selenium Grid

--- src/test/java/WebDriverManager.java
+++ src/test/java/WebDriverManager.java
@@ -1,11 +1,19 @@
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebDriverManager {
 
-    public static ChromeDriver chromeDriver(){
-        System.setProperty("webdriver.chrome.driver" , "src/test/resources/chromedriver.exe");
-        return new ChromeDriver();
+//    public static ChromeDriver chromeDriver(){
+//        System.setProperty("webdriver.chrome.driver" , "src/test/resources/chromedriver.exe");
+//        return new ChromeDriver();
 
-
+//    }
+    public static RemoteWebDriver getRemoteWebDriver() throws MalformedURLException {
+        return new RemoteWebDriver(new URL("http://localhost:4444/"), new ChromeOptions());
     }
+
 }
